cdb330ca0aba74c40cbdc90280dcf2d0b14a88dd
mlgarchery <martial.garchery@gmail.com>
2021-04-30 01:20:16 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <h3>Memento mori</h3>
+    <div class="monthes">
+      <div 
+        v-for="month in monthes"
+        :key="month"
+        :class="['month', {['year']: month%120==0}]" >
+      {{ month }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  computed: {
+    monthes() {
+      let result = [];
+      for(let i=1; i <= 90*12; i++){
+        result.push(i);
+      }
+      return result;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+$page_padding: 20px;
+$square_size: 25px;
+$font_size: 10px;
+$square_margin: 4px;
+$number_of_square_on_a_row: 36; // 2 ans
+$app_width: $number_of_square_on_a_row * ($square_size + 2 * $square_margin);
+
+body {
+  display: flex;
+  justify-content: center;
+  margin: 0;
+  padding: $page_padding;
+
+}
+#app {
+  display: flex;
+  flex-flow: column;
+  width: $app_width;
+  padding: 0;
+  > h3 {
+    margin-bottom: 10px;
+    text-align: center;
+  }
+}
+.monthes {
+  display: flex;
+  flex-flow: row wrap;
+}
+.month{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: $square_size;
+  height: $square_size;
+  margin: $square_margin;
+  font-size: $font_size;
+  background-color: white;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+.year {
+  // background-color: blue;
+  // background-color: black;
+  border: 2px solid black;
+}
+</style>
